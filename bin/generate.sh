2d0a7edd1074a850c2ec8d46141fae22fba99149
wansong <betterofsong@gmail.com>
2020-07-12 23:26:22 +0800

    feat: better this_file resolving

--- bin/generate.sh
+++ bin/generate.sh
@@ -1,12 +1,25 @@
 #!/usr/bin/env bash
 set -eu
 
-if [[ "$0" =~ ^/ ]] ; then
-  this_file="$0"
-else
-  this_file="$(pwd)/$0"
+this_file="$(type -P "${BASH_SOURCE[0]}")"
+if ! [ -e "$this_file" ] ; then
+  echo "Failed to resolve file."
+  exit 1
 fi
+if ! [[ "$this_file" =~ ^/ ]] ; then
+  this_file="$(pwd)/$this_file"
+fi
+while [ -h "$this_file" ] ; do
+    ls_res="$(ls -ld "$this_file")"
+    link_target=$(expr "$ls_res" : '.*-> \(.*\)$')
+    if [[ "$link_target" =~ ^/ ]] ; then
+      this_file="$link_target"
+    else
+      this_file="$(dirname "$this_file")/$link_target"
+    fi
+done
 this_dir="$(dirname "$this_file")"
+
 src_dir="${this_dir}/.."
 
 declare -a list_of_files=()
